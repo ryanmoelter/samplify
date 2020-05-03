8a78f3c9a95b62a770c5c94ceb422356637b6cb1
Ryan Moelter <ryanmoelter@gmail.com>
2020-05-03 13:46:55 -0700

    Initial commit

---
+++ app/src/main/java/co/moelten/samplify/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.moelten.samplify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
