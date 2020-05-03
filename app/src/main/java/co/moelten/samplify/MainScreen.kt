f389712126fa20edaa8a6e23f83ff55dd02acb9b
Ryan Moelter <ryanmoelter@gmail.com>
2020-05-03 15:36:59 -0700

    Create basic mainScreen

--- app/src/main/java/co/moelten/samplify/MainScreen.kt
+++ app/src/main/java/co/moelten/samplify/MainScreen.kt
@@ -2,6 +2,8 @@ package co.moelten.samplify
 
 import android.content.Context
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.wealthfront.magellan.BaseScreenView
 import com.wealthfront.magellan.Screen
 import com.wealthfront.magellan.ScreenView
@@ -10,4 +12,11 @@ class MainScreen : Screen<MainView>() {
   override fun createView(context: Context): MainView = MainView(context)
 }
 
-class MainView(context: Context) : BaseScreenView<MainScreen>(context)
+class MainView(context: Context) : BaseScreenView<MainScreen>(context) {
+  var nowPlayingAlbumArt: ImageView by bindView(R.id.nowPlayingAlbumArt)
+  var nowPlayingTitle: TextView by bindView(R.id.nowPlayingTitle)
+
+  init {
+    inflate(R.layout.main_screen)
+  }
+}
